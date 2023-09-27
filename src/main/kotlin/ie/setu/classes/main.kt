5e97e6185324f9525b671440c3295064a9c048dc
JGJordan <20106285@mail.wit.ie>
2023-09-27 15:59:23 +0100

    Add uppercase last name

--- src/main/kotlin/ie/setu/classes/main.kt
+++ src/main/kotlin/ie/setu/classes/main.kt
@@ -1,6 +1,16 @@
 package ie.setu.classes
 
-fun main(args: Array<String>) {
+private val persons = ArrayList<Person>()
+fun main(args: Array<String>){
+    persons.add(Person("Mark","Roche"))
+    persons.add(Person("John", "Walsh"))
+    persons.add(Person("Sheila", "Flynn"))
+    personalInfo()
+
+    for ((index, value) in persons.withIndex())
+        println("$index: $value")
+}
+fun personalInfo() {
     val person1 = Person("Mark", "Roche")
     val person2 = person1.copy(firstName = "Clare")
     val person3 = person1.copy()
@@ -18,6 +28,13 @@ fun main(args: Array<String>) {
         println("Person 1 equals person 3")
     else
         println("Person 1 does not equal person 3")
+
+    println("\nUsing Lambdas:\n")
+    println("\nFiltering first name as John: ")
+    persons
+        .sortedBy {it.lastName}
+        .forEach {println("Person : ${it.firstName}, ${it.lastName.uppercase()}")}
+
 
 }
 
